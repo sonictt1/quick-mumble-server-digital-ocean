0ba7708d1a60f579bd9c1518cb023a238da4780e
Nic <sonictt1@proton.me>
2026-03-11 20:33:07 -0600

    added subdomain as a customizable parameter

--- letsencrypt_cert_script.sh
+++ letsencrypt_cert_script.sh
@@ -33,6 +33,10 @@ while [[ $# -gt 0 ]]; do
             ADMIN_USERNAME="$2"
             shift; shift
             ;;
+        --subdomain)
+            SUBDOMAIN="$2"
+            shift; shift
+            ;;
         *)
             echo "Unknown option: $1"
             exit 1
@@ -40,14 +44,16 @@ while [[ $# -gt 0 ]]; do
     esac
 done
 
-if [[ -z "$RESERVED_IP" || -z "$DROPLET_ID" || -z "$DOMAIN" || -z "$EMAIL" || -z "$SSH_IDENTITY_FILE" || -z "$SSH_PORT" ]]; then
+if [[ -z "$RESERVED_IP" || -z "$DROPLET_ID" || -z "$DOMAIN" || -z "$EMAIL" || -z "$SSH_IDENTITY_FILE" || -z "$SSH_PORT" || -z "$ADMIN_USERNAME" || -z "$SUBDOMAIN" ]]; then
     echo "RESERVED_IP=$RESERVED_IP"
     echo "DROPLET_ID=$DROPLET_ID"
     echo "DOMAIN=$DOMAIN"
     echo "EMAIL=$EMAIL"
     echo "SSH_IDENTITY_FILE=$SSH_IDENTITY_FILE"
     echo "SSH_PORT=$SSH_PORT"
-    echo "Usage: $0 --reserved-ip <RESERVED_IP> --droplet-id <DROPLET_ID> --domain <DOMAIN> --email <EMAIL> --ssh-identity-file <SSH_IDENTITY_FILE> --ssh-port <SSH_PORT>"
+    echo "ADMIN_USERNAME=$ADMIN_USERNAME"
+    echo "SUBDOMAIN=$SUBDOMAIN"
+    echo "Usage: $0 --reserved-ip <RESERVED_IP> --droplet-id <DROPLET_ID> --domain <DOMAIN> --email <EMAIL> --ssh-identity-file <SSH_IDENTITY_FILE> --ssh-port <SSH_PORT> --admin-username <ADMIN_USERNAME> --subdomain <SUBDOMAIN>"
     exit 1
 fi
 
@@ -62,10 +68,10 @@ ssh $SSH_OPTS $ADMIN_USERNAME@$DROPLET_IP <<'EOF'
     DEBIAN_FRONTEND=noninteractive sudo apt-get install -yq --no-install-recommends -o Dpkg::Options::="--force-confold" certbot
     sudo systemctl stop mumble-server || true
 
-    sudo certbot certonly --standalone -d "mumble.$DOMAIN" --non-interactive --agree-tos --email "$EMAIL"
+    sudo certbot certonly --standalone -d "$SUBDOMAIN.$DOMAIN" --non-interactive --agree-tos --email "$EMAIL"
 
-    sudo sed -i "/^sslCert=/c\\sslCert=/etc/letsencrypt/live/mumble.$DOMAIN/fullchain.pem" "/etc/mumble-server.ini"
-    sudo sed -i "/^sslKey=/c\\sslKey=/etc/letsencrypt/live/mumble.$DOMAIN/privkey.pem" "/etc/mumble-server.ini"
+    sudo sed -i "/^sslCert=/c\\sslCert=/etc/letsencrypt/live/$SUBDOMAIN.$DOMAIN/fullchain.pem" "/etc/mumble-server.ini"
+    sudo sed -i "/^sslKey=/c\\sslKey=/etc/letsencrypt/live/$SUBDOMAIN.$DOMAIN/privkey.pem" "/etc/mumble-server.ini"
 
     sudo systemctl start mumble-server || true
 
