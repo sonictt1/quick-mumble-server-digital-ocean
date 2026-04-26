a72335562b64fddc1bd522cb27940275c4f7e356
Nic <sonictt1@proton.me>
2026-04-25 18:42:04 -0600

    firewal adjustment

--- inflate_murmur.sh
+++ inflate_murmur.sh
@@ -392,12 +392,13 @@ PUBKEY
     ufw --force reset
     ufw default deny incoming
     ufw default allow outgoing
-    ufw allow 22/tcp
+    ufw allow $ssh_port/tcp
     ufw allow 80/tcp
     ufw allow 443/tcp
     ufw allow $MUMBLE_PORT/tcp
     ufw allow $MUMBLE_PORT/udp
     ufw --force enable
+    ufw status verbose
 
     # Final sync and reboot to apply all changes
     sync
